ed6f486868b962550fef2e3947d66920626b36e2
JMonteiroh <joao.paulomonteiro@hotmail.com>
2022-12-20 12:43:04 -0300

    events,js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 import Timer from "./timer.js";
 import Controls from "./controls.js";
 import Sound from "./sounds.js";
+import Events from "./events.js";
 import {
   buttonPause,
   buttonSet,
   buttonPlay,
-  buttonSoundOff,
-  buttonSoundOn,
   buttonStop,
   minutesDisplay,
   secondsDisplay
@@ -26,47 +25,6 @@ const timer = Timer({
 
 const sound = Sound()
 
+Events({controls, timer, sound})
 
-buttonPlay.addEventListener('click', function() {
-  controls.play()
-  timer.countdown()
-  sound.pressButton()
 
-});
-
-buttonPause.addEventListener('click', function() {
-  controls.pause()
-  timer.hold()
-  sound.pressButton()
-
-});
-
-buttonStop.addEventListener('click', function() {
-  controls.reset()
-  timer.reset()
-  sound.pressButton()
-
-});
-
-buttonSoundOn.addEventListener('click', function() {
-  buttonSoundOn.classList.add('hide');
-  buttonSoundOff.classList.remove('hide');
-  sound.bgAudio.play()
-});
-
-buttonSoundOff.addEventListener('click', function() {
-  buttonSoundOn.classList.remove('hide');
-  buttonSoundOff.classList.add('hide');
-  sound.bgAudio.pause()
-});
-
-buttonSet.addEventListener('click', function() {
-  let newMinutes = controls.getMinutes()
-  if (!newMinutes) {
-    timer.reset()
-    return
-  }
-  timer.updateDisplay(newMinutes, 0)
-  timer.updateMinutes(newMinutes)
-
-})
